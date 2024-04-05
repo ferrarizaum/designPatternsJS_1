0b2507edb70a122e4fbe54b7189bd6f9457a7e0f
José Ferrari <licoverpa321@gmail.com>
2024-04-04 21:03:01 -0300

    adding tests

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,25 @@ import { Contato } from "./controllers/contato.mjs";
 import { GerenciadorContato } from "./controllers/gerenciadorContatos.mjs";
 
 const testaContato = new Contato();
+const testaContato2 = new Contato();
 
 testaContato.setNome("Jose Ferrari");
 testaContato.setEmail("joseferrari@email.com");
 testaContato.setTelefone("43991250895");
 
+testaContato2.setNome("teste");
+testaContato2.setEmail("teste@email.com");
+testaContato2.setTelefone("12345678");
+
 const testaListaContatos = new GerenciadorContato();
 
-testaListaContatos.setListaContatos(contato);
+testaListaContatos.setListaContatos(testaContato);
+testaListaContatos.setListaContatos(testaContato2);
+
+console.log(testaContato.getNome());
+console.log(testaContato.getEmail());
+console.log(testaContato.getTelefone());
+
+console.log("\n");
 
 console.log(testaListaContatos.getListaContatos());
